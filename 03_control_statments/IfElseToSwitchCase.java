c249fa76d5d35f1f1060b3560c403d22555d9def
rafi0012 <rafiultalukdar007@gmail.com>
2026-07-06 15:35:37 +0600

    Solve first switch case

--- 03_control_statments/IfElseToSwitchCase.java
+++ 03_control_statments/IfElseToSwitchCase.java
@@ -1,7 +1,36 @@
 public class IfElseToSwitchCase {
     public static void main(String[] args) {
 
+        // ********************************** problem 1: **********************************
+        // initialize a variable with int value, if the variable is even then print even
+        // and if the variable is odd then print odd.
+        // ********************************************************************************
+
+        int number = 12;
+
+        switch ((number % 2)) {
+            case 0:
+                System.out.println("Even");
+                break;
+            default:
+                System.out.println("Odd");
+        }
+
+        // ********************************** problem 2: **********************************
+        // initialize an integer n and determines whether it is negative, even, or odd.
+        // ********************************************************************************
+
+
+
+
+
+
+
         
+
+
+
+
 
     }
 }
