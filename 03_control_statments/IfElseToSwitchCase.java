c2f42b7a33f0d9cdb7071f45a195a4e30cadb5f5
rafi0012 <rafiultalukdar007@gmail.com>
2026-07-07 10:28:17 +0600

    Explore IfElseToSwitchCase

--- 03_control_statments/IfElseToSwitchCase.java
+++ 03_control_statments/IfElseToSwitchCase.java
@@ -20,10 +20,67 @@ public class IfElseToSwitchCase {
         // initialize an integer n and determines whether it is negative, even, or odd.
         // ********************************************************************************
 
+        int num = 3;
 
+        // if (num >= 0) {
+        //      if ((num % 2) == 1) {
+        //          System.out.println("odd");
+        //      }
+        //      else {
+        //          System.out.println("even");
+        //      }
+        // }
+        // else {
+        //     System.out.println("Negative");
+        // }
 
+        switch (Integer.signum(num)) {
+            case 0:
+            case 1:
+                switch ((num % 2)) {
+                    case 0:
+                        System.out.println("Number: " + num);
+                        System.out.println("Status: Positive Even");
+                        break;
+                    default:
+                        System.out.println("Number: " + num);
+                        System.out.println("Status: Positive Odd");
+                }
+                break;
+            default:
+                System.out.println("Number: " + num);
+                System.out.println("Status: Negative");
+                break;
+        }
 
+        // ********************************** problem 3: **********************************
+        // initialize a variable mark int value. and print the greade according,
+        // mark >= 90 -> A
+        // mark < 90 and mark >= 80 -> A-
+        // mark < 80 and mark >= 70 -> B
+        // mark < 70 and mark >= 60 -> B-
+        // else F
+        // ********************************************************************************
 
+        int mark = 90;
+
+        switch ((mark / 10)) {
+            case 1:
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("A-");
+                break;
+            case 7:
+                System.out.println("B");
+                break;
+            case 6:
+                System.out.println("B-");
+                break;
+            default:
+                System.out.println("F");
+        }
 
 
         
