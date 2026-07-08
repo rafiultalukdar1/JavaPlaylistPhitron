7c6e1ccc4382aaa4a141b773d310a3d1f59fc10e
rafi0012 <rafiultalukdar007@gmail.com>
2026-07-08 22:29:22 +0600

    Add another example

--- 03_control_statments/Loop.java
+++ 03_control_statments/Loop.java
@@ -9,9 +9,35 @@ public class Loop {
             System.out.println(i * 2);
         }
 
+        int im = 1;
+        while (im <= 10) {
+            System.out.print(im*2 + " ");
+            im++;
+        }
 
 
+        // * problem 2:
+        // * --------------------------------
+        // * FizzBuzz problem
+        // * print numbers from 1 to 20
+        // * if the number is divisible by 3, print "Fizz"
+        // * if the number is divisible by 5, print "Buzz"
+        // * if the number is divisible by 3 and 5, print "FizzBuzz"
+        // * else print the number
 
+        for (int i=1; i <=20; i++) {
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+            }
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
+            } else {
+                System.out.println(); // \n -> new line
+            }
+        }
 
 
 
