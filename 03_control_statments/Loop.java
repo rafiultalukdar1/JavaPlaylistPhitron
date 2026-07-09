e0669b682ec470aa46b02a409cef6fb67cb9eb7a
rafi0012 <rafiultalukdar007@gmail.com>
2026-07-09 14:25:34 +0600

    Solve another problem

--- 03_control_statments/Loop.java
+++ 03_control_statments/Loop.java
@@ -57,6 +57,27 @@ public class Loop {
             System.out.println();
         }
 
+        // * problem 4:
+        // * -----------------------------------
+        // * print the following pattern for n=5
+        // * 1
+        // * 1 2
+        // * 1 2 3
+        // * 1 2 3 4
+        // * 1 2 3 4 5
+
+        System.out.println("Problem 04 Solve");
+
+        for (int r=1; r<=5; r++) {
+            for (int c=1; c<=r; c++) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
 
 
 
